ed1695cf17cf8969b068b254c4915cf04dd2e5a2
Majd ALfi <majdsomer@gmail.com>
2022-07-31 16:51:08 +0300

    Adding Login And Sign up Screens

--- lib/src/screens/sign_in_screen.dart
+++ lib/src/screens/sign_in_screen.dart
@@ -5,6 +5,78 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Container(
+                height: 60,
+                width: 300,
+                decoration: BoxDecoration(color: Colors.grey),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 60,
+                width: 300,
+                decoration: BoxDecoration(color: Colors.grey),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 140.0),
+                child: TextButton(
+                    onPressed: () {}, child: Text("Forgot Your Password?")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(color: Colors.grey),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      )))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/src/screens/sign_up_screen.dart
+++ lib/src/screens/sign_up_screen.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:open_vacancies/src/screens/sign_in_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 200),
+                child: Container(
+                    height: 50,
+                    width: 100,
+                    decoration: BoxDecoration(color: Colors.grey),
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignInScreen()));
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ))),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "REGISTER",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Container(
+                height: 60,
+                width: 300,
+                decoration: BoxDecoration(color: Colors.grey),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 60,
+                width: 300,
+                decoration: BoxDecoration(color: Colors.grey),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(color: Colors.grey),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white),
+                      )))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
